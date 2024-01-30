80464105b44624267b5673da7fc2666e0e9f8fc9
Your Name01~ <you@example.com>
2024-01-30 12:22:46 +0530

    Fourth commit

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading("Loading...");
+        setData(null);
+        setError(null);
+
+        fetchDataFromApi(url)
+            .then((res) => {
+                setLoading(false);
+                setData(res);
+            })
+            .catch((error) => {
+                setError(error);
+                setLoading("Something went wrong!");
+            })
+    }, [url]);
+    return (data, loading, error);
+}
--- src/pages/home/homeBanner/HomeBanner.jsx
+++ src/pages/home/homeBanner/HomeBanner.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-import "./homeBanner.scss"
+import React, { useState } from 'react';
+import "./homeBanner.scss";
+import { useNavigate } from "react-router-dom"
+
 const HomeBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  
+  const searchQueryHandler = (event) => {
+    if(event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  }  
   return (
-    <div>HomeBanner</div>
+    <div className="homeBanner">
+      <div className="wrapper">
+        <div className="homeBannerContent">
+          <span className='subTitle'>
+            Discover popular Movies,
+            TV shows and more
+          </span>
+          <div className="serachInput">
+            <input type='text' placeholder='Search for a movie or TV show...' onChange={(e) => setQuery(e.target.value)} onKeyUp={searchQueryHandler}/>
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
